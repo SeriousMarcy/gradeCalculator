28d3b2184cf2c70af4ade56fce10aa87cc63cefd
Marcy <marcelax2918@gmail.com>
2017-07-16 21:20:51 +1000

    adding menu options

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+#!/usr/bin/python
+
 import python
 
+def main():
+
+    print "Welcome to the grade calculator"
+        + "1. Add a class"
+        + "2. Add an assessment"
+        + "3. View your stats"
 
 
 
+if __name__ == '__main__':"
+    main()
